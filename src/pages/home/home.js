bcbcfb64fb31290a7ae4066e63599872bff75145
Denis <denisalalykin20012gmail.com>
2022-11-13 09:44:20 +0700

    fix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,18 +5,21 @@ import { getAuth, signOut } from 'firebase/auth'
 function Home() {
 	const { isAuth, email } = useAuth()
 	const auth = getAuth()
-	signOut(auth)
-		.then(() => {
-			console.log('lol')
-			// Sign-out successful.
-		})
-		.catch((error) => {
-			// An error happened.
-		})
+	const out = () => {
+		signOut(auth)
+			.then(() => {
+				console.log('lol')
+				console.log(email)
+				// Sign-out successful.
+			})
+			.catch((error) => {
+				// An error happened.
+			})
+	}
 	return isAuth ? (
 		<div>
 			{' '}
-			<h1> hello, {email}</h1> <Button onClick={signOut}>SignOut</Button>
+			<h1> hello, {email}</h1> <Button onClick={out}>SignOut</Button>
 		</div>
 	) : (
 		<Navigate to="/login" />
