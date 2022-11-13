4a222f149cf94d17acb92a544cddaf1f0b61f92d
Denis <denisalalykin20012gmail.com>
2022-11-13 09:41:13 +0700

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
+	apiKey: 'AIzaSyDq2FhyhaIV2FLHhvfPv2CWcnRTUSCTGqA',
+	authDomain: 'mydiplomesite.firebaseapp.com',
+	projectId: 'mydiplomesite',
+	storageBucket: 'mydiplomesite.appspot.com',
+	messagingSenderId: '414347545630',
+	appId: '1:414347545630:web:42bed2917887e5049b720f',
+}
 
-const app = initializeApp(firebaseConfig);
-export default app;
+const app = initializeApp(firebaseConfig)
+export default app
