845b0a3e815f23126315b89788e78a3c88deafc1
Denis <denisalalykin20012gmail.com>
2022-11-14 21:39:46 +0700

    fixBug

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
-import  AppBarHome  from '../../components/appBarHome/appBarHome';
 import { Button } from 'rsuite';
 import { AuthContext } from '../../context/AuthContext';
-
+import AppBarHome from '../../components/appBarHome/appBarHome';
 function Home() {
   const { currentUser } = useContext(AuthContext);
   var obj = JSON.stringify(currentUser);
@@ -10,6 +9,7 @@ function Home() {
   return (
     <>
       <AppBarHome />
+      <br />
       <h1>{objectJSON.email}</h1>
     </>
   );
