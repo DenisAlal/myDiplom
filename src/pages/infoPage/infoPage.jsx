2ed986b83b0de18ca1a5cf4588abdc3d577cfcd8
Denis <denisalalykin20012gmail.com>
2022-11-24 14:43:27 +0700

    addInfo

--- src/pages/infoPage/infoPage.jsx
+++ src/pages/infoPage/infoPage.jsx
@@ -1,15 +1,29 @@
 import AppBarLog from '../../components/appBar/appBar';
+import Image from 'mui-image';
 import './infoPage.scss';
-
+import BG from './BG.png';
+import { Grid, Paper } from '@mui/material';
 function Start() {
   return (
     <>
       <AppBarLog goToHome={'/'} type={'info'} />
       <main>
-        <div className='info_block'>
-          <h4>
+        <div className='container'>
+          <div className='centerBG'>
+            <Image
+              className='image'
+              src={BG}
+              bgColor='inherit'
+              shiftDuration={500}
+              fit='scale-down'
+              height='90%'
+              width='80%'
+            />
+          </div>
+
+          <div className='divText'>
             Это иноформация о сайте, этот сайт ультра мега крутышка пушка гонка
-          </h4>
+          </div>
         </div>
       </main>
     </>
