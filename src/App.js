1c0c3d4d935ae470f9506f9c28dd42d34d3c844c
Denis <denisalalykin2001@gmial.com>
2022-12-26 23:05:49 +0700

    smallFix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.scss';
 import { useContext } from 'react';
-import {
-  Routes,
-  Route,
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import InfoPage from './pages/infoPage/infoPage';
 import Login from 'pages/login/login';
-import Home, { homeloader } from 'pages/home/home';
+import Home from 'pages/home/home';
 import NotFound from 'pages/page404/page404';
 import { AuthContext } from 'context/AuthContext';
 import QRscanner from 'pages/qrScanner/qrScanner';
+
+// ctrl + shift + e explorer
+// ctrl + shift + x extensions
+// ctrl + shift + f finder
+// ctrl + shift + g git
+// ctrl + b убрать все
+
 function App() {
   const { currentUser } = useContext(AuthContext);
   const RequireAuth = ({ children }) => {
@@ -35,11 +35,17 @@ function App() {
             </RequireAuth>
           }
         />
-        <Route path='qrscanner' element={<RequireAuth><QRscanner/></RequireAuth>}/>
+        <Route
+          path='qrscanner'
+          element={
+            <RequireAuth>
+              <QRscanner />
+            </RequireAuth>
+          }
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
